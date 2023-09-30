9154804f2d85d8fcd2e5ea646b4cd43126847f94
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-09-30 02:20:02 -0300

    add: first query

---
+++ client/components/SongList.js
@@ -0,0 +1,30 @@
+import React from "react"
+import gql from "graphql-tag"
+import { graphql } from "react-apollo"
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`
+
+const SongList = ({ data }) => {
+  console.log(data)
+
+  if (data.loading) {
+    return <div>Loading</div>
+  } else {
+    return (
+      <ul>
+        {data.songs.map(({ id, title }) => {
+          return <li key={id}>{title}</li>
+        })}
+      </ul>
+    )
+  }
+}
+
+export default graphql(query)(SongList)
